361b3357337a4256593c60b9bd89d2178c44fbaf
unknown <grabarskiy2002@mail.ru>
2017-09-06 21:05:30 +0300

    first commit

--- main.cpp
+++ main.cpp
@@ -10,17 +10,12 @@ using namespace std;
 int main()
 {
     int a, b, c;
-    int r;
-    int temp;
-    cout << "Enter first number: ";
-    cin >> a;
-    cout << "Enter second number: ";
-    cin >> b;
-    c = a + b;
-    temp = c;
-    c = r;
-    r = temp;
-    cout << r << endl;
+    a = 5;
+    b = 7;
+    c = a;
+    cout << a << " " << b << endl;
+    a = b;
+    cout << a << " " << c << endl;
 
     return 0;
 }
