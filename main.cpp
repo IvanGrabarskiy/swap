76b39b113341246ade0f95255ffd6a7c503f584a
unknown <grabarskiy2002@mail.ru>
2017-09-06 20:54:40 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <typeinfo>
+#include <math.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    int r;
+    int temp;
+    cout << "Enter first number: ";
+    cin >> a;
+    cout << "Enter second number: ";
+    cin >> b;
+    c = a + b;
+    temp = c;
+    c = r;
+    r = temp;
+    cout << r << endl;
+
+    return 0;
+}
